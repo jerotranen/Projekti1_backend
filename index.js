7748e492598155b708840ad7b41c803f48f24d8e
jerotranen <jerotranen@gmail.com>
2024-02-27 12:48:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./config')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const logger = require('./loggers')
+const User = require('./user')
+const usersRouter = require('./logins/usersRouter');
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.static('build'))
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(cors())
+app.use('/users', usersRouter);
+
+
+// MONGOOSE
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+.then(() => {
+    logger.info('connected To MongoDB') 
+    })
+    .catch((error) => {
+        logger.error('error connection to MongoDB:', error.message)
+    })
+
+    app.get('/users', async (req, res) => {
+        const users = await User.find({});
+        res.json(users);
+      });
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
+
+module.exports = app;
