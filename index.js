0804bddb87efa4d10d2e9bdafc15f062912a0501
Jeremias Otranen <jerotranen@gmail.com>
2024-03-12 09:44:40 -0700

    cors

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const cors = require('cors')
+app.use(cors())
 const http = require('http')
 const express = require('express')
 const mongoose = require('mongoose')
@@ -10,22 +12,18 @@ const usersRouter = require('./logins/usersRouter');
 const loginRouter = require('./logins/login')
 const formRouter = require('./misc/formRouter');
 const morgan = require('morgan')
-const cors = require('cors')
 const ilmoStatusRouter = require('./misc/isopenrouter');
 const imageRouter = require('./misc/imageRouter');
-
 const app = express();
 
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny'))
-app.use(cors())
 app.use('/users', usersRouter)
 app.use('/login', loginRouter)
 app.use('/ilmot', formRouter);
 app.use('/status', ilmoStatusRouter);
 app.use('/image', imageRouter);
-
 
 // MONGOOSE
 logger.info('connecting to', config.MONGODB_URI)
